00b8846b9c39b3c7d686f7a60787aec307b90f38
陈桐 <chentong01@hexindai.com>
2020-07-13 10:03:08 +0800

    hide

--- lib/test/net_test.dart
+++ lib/test/net_test.dart
@@ -7,7 +7,7 @@ import 'package:fluttertoast/fluttertoast.dart';
 class TestNet {
   ///测试登录接口
   static void testLogin() async {
-    await GithubRepository.login("704514698@qq.com", "Heat1314").then((baseResp) {
+    await GithubRepository.login("xxxxxxx@qq.com", "xxxxxx").then((baseResp) {
       Fluttertoast.showToast(msg: "用户id" + baseResp.data.id.toString());
     }).catchError((err) {
       DioError dioError = err as DioError;
